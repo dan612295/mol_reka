a0f2b0fff4a5ec4c66b11918ce99900db1cfcb5c
dan612295 <96869412+dan612295@users.noreply.github.com>
2021-12-30 19:14:32 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,5 @@
+let historyBlock=document.getElementsByClassName("txt")[0];
+let photoFarm=document.getElementsByClassName("img1")[0];
+function resizeHistory() {
+    historyBlock.style.height=photoFarm.offsetHeight+"px";  
+}
---
+++ aboutComp.js
@@ -0,0 +1,3 @@
+let cowsBlock=document.getElementsByClassName("img-row")[0];
+let videosBlock=document.getElementsByClassName("element_row")[0];
+cowsBlock.style.height=videosBlock.offsetHeight+"px";
